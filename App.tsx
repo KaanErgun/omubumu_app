99a832cb3c3c25454ca64aca2dd79f584a1e9c7a
KaanErgun <kaanergun@live.com>
2024-02-04 22:28:48 +0300

    hamburger menu added

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
+import 'react-native-gesture-handler';
 import React, {useState} from 'react';
 import {View, Text, TouchableOpacity, StyleSheet, Dimensions, SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 const {height} = Dimensions.get('window');
 
-const App = () => {
+function ChoiceScreen() {
   // Kullanıcı tercihlerini tutacak state'ler
   const [choice1Count, setChoice1Count] = useState(0);
   const [choice2Count, setChoice2Count] = useState(0);
@@ -19,17 +22,42 @@ const App = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <TouchableOpacity style={[styles.choice, {backgroundColor: '#ADD8E6', marginVertical: 20}]} onPress={onChooseOption1}>
-          <Text style={styles.choiceText}>Tercih 1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.choice, {backgroundColor: '#FFB6C1', marginVertical: 20}]} onPress={onChooseOption2}>
-          <Text style={styles.choiceText}>Tercih 2</Text>
-        </TouchableOpacity>
-      </View>
+      <TouchableOpacity style={[styles.choice, {backgroundColor: '#ADD8E6', marginVertical: 20}]} onPress={onChooseOption1}>
+        <Text style={styles.choiceText}>Tercih 1</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.choice, {backgroundColor: '#FFB6C1', marginVertical: 20}]} onPress={onChooseOption2}>
+        <Text style={styles.choiceText}>Tercih 2</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
-};
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Ayarlar</Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator initialRouteName="ChoiceScreen">
+      <Drawer.Screen name="ChoiceScreen" component={ChoiceScreen} options={{drawerLabel: 'Tercih Ekranı'}} />
+      <Drawer.Screen name="Settings" component={SettingsScreen} options={{drawerLabel: 'Ayarlar'}} />
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -50,5 +78,3 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
-
-export default App;
